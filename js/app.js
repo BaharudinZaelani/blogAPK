23d8727c58e69607ea16264acfac53b69fe8732f
Zaelani <66102282+BaharudinZaelani@users.noreply.github.com>
2022-02-01 16:50:42 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -8,8 +8,9 @@ console.log('More Info            : https://baharudinzaelani.github.io/template/
 $(".sparator a img").addClass("img-fluid")
 $(document).ready(() =>
 {
-	let c = $('a#copyright';).text();
-	if(c == 'NgaCode'){}else {
+	let c = $('a#copyright').text();
+	if(c == 'NgaCode'){
+	}else {
 		window.location.href = 'https://ngacode.blogspot.com/2022/02/az-template.html'
 	}
 
