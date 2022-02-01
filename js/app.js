b56e154bcf9ce26b728dc257fefd683d5676407f
Zaelani <66102282+BaharudinZaelani@users.noreply.github.com>
2022-02-01 15:20:32 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,66 +1,86 @@
 
-    $(".sparator a img").addClass("img-fluid")
-$(document).ready(() => {
-    // started
-    if( $.cookie('dark') ){
-        $('#dark').attr("checked", "true")
-        dark(true)
-    }else {
-        $('#dark').removeAttr("checked")
-        dark(false)
-    }
+console.log('%c TEMPLATE INFO ', 'color: red; background-color: yellow; font-size: 20px;')
+console.log('Template Name        : AndroZaw Template')
+console.log('Uploaded in          : Ngacode')
+console.log('Designed by          : Baharudin Zaelani')
+console.log('More Info            : https://baharudinzaelani.github.io/template/androzaw.html')
+
+$(".sparator a img").addClass("img-fluid")
+$(document).ready(() =>
+{
+	// started
+	if ($.cookie('dark'))
+	{
+		$('#dark').attr("checked", "true")
+		dark(true)
+	}
+	else
+	{
+		$('#dark').removeAttr("checked")
+		dark(false)
+	}
 
 
 
-    // dark mode
-    $('#dark').click(()=>{
-        const dark = document.getElementById('dark')
-        if( dark.checked == true ){
-            $.cookie("dark", "true");
-        }else {
-            $.removeCookie('dark');
-        }
-        location.reload();
-    })
+	// dark mode
+	$('#dark').click(() =>
+	{
+		const dark = document.getElementById('dark')
+		if (dark.checked == true)
+		{
+			$.cookie("dark", "true");
+		}
+		else
+		{
+			$.removeCookie('dark');
+		}
+		location.reload();
+	})
 
-    function dark(cond){
-        if( cond ){
-            // dark mode
-            $('body').toggleClass('body-dark')
+	function dark(cond)
+	{
+		if (cond)
+		{
+			// dark mode
+			$('body').toggleClass('body-dark')
 
-            // navbar dark
-            $('.navbar').toggleClass('navbar-light')
-            $('.navbar').toggleClass('bg-light')
-            $('.navbar').toggleClass('navbar-dark')
-            $('.navbar').toggleClass('bg-dark')
-            $('.dropdown-menu').css("background-color", "#212529")
+			// navbar dark
+			$('.navbar').toggleClass('navbar-light')
+			$('.navbar').toggleClass('bg-light')
+			$('.navbar').toggleClass('navbar-dark')
+			$('.navbar').toggleClass('bg-dark')
+			$('.dropdown-menu').css("background-color", "#212529")
 
-            // title
-            $('a.navbar-brand').toggleClass('navbar-brand-dark')
+			// title
+			$('a.navbar-brand').toggleClass('navbar-brand-dark')
 
-            // search
-            $('.offcanvas').css('background', '#111')
+			// search
+			$('.offcanvas').css('background', '#111')
 
-            // template
-            $('.bg-light').toggleClass('bg-light')
-            $('.bg-light').toggleClass('bg-dark')
-            $('a').css('color', 'white')
+			// template
+			$('.bg-light').toggleClass('bg-light')
+			$('.bg-light').toggleClass('bg-dark')
+			$('a').css('color', 'white')
 
-            $('.content').css('color', 'white !important')
+			$('.content').css('color', 'white !important')
 
-        }
-    }   
+		}
+	}
 
 
-    // scroll
-    $(window).scroll(() => {
-        let scroll = $(window).scrollTop()
-        if( scroll >= 200 ){
-            $('.totop').css('bottom', '81px')
-        }else {
-            $('.totop').css('bottom', '-112px')
-        }
-    })
+	// scroll
+	$(window).scroll(() =>
+	{
+		let scroll = $(window).scrollTop()
+		if (scroll >= 200)
+		{
+			$('.totop').css('bottom', '81px')
+		}
+		else
+		{
+			$('.totop').css('bottom', '-112px')
+		}
+	})
 
 
 })
