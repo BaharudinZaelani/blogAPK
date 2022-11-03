66e628277256bc288472a0cfde324ceb0cd2b7a0
Zaelani <bahar32harz@gmail.com>
2022-11-03 18:34:09 +0700

    responsive for phone

--- js/androzaw.app.js
+++ js/androzaw.app.js
@@ -1,5 +1,6 @@
 $(window).scroll(function (event) {
     let scroll = $(window).scrollTop()
+    console.log('zaw');
     if ( scroll > 47 ) {
         $("header").addClass("header-scroll-modif")
     }else {
