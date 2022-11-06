5b00fe38c1a0827f1716129badde961ea6b41242
Zaelani <bahar32harz@gmail.com>
2022-11-07 01:16:54 +0700

    calling api endpoint makes it flexibel

--- js/androzaw.app.js
+++ js/androzaw.app.js
@@ -1,9 +1,22 @@
 $(window).scroll(function (event) {
     let scroll = $(window).scrollTop()
-    console.log('zaw');
     if ( scroll > 47 ) {
         $("header").addClass("header-scroll-modif")
     }else {
         $("header").removeClass("header-scroll-modif")
     }
 });
+
+$(document).ready(()=> {
+    // get project
+    axios.get("https://zawtea.000webhostapp.com/api/v1/project").then(function(res){
+        // res.data
+        // console.log(res.data.data[0]);
+        $("#loop").html("")
+        // console.log(Object.keys(res.data.data).length);
+        for( let i = 0; i < Object.keys(res.data.data).length;) {
+            $("#loop").append('<a href="'+ res.data.data[i].link +'" target="_blank" class="card"> <img src="'+ res.data.data[i].image +'" alt="'+ res.data.data[i].name +'" class="img-fluid"> <div class="card-body"> <h2>'+ res.data.data[i].name +'</h2> <p>'+ res.data.data[i].description +'</p> </div> </a>');
+            i++;
+        }
+    });
+})
