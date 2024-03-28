7398e0d17e1e4f5b10665f64c14d72126df147ae
BaharudinZaelani <bahar32harz@gmail.com>
2024-03-28 20:16:20 +0700

    [Zawhax] Update Style

--- js/zawhax.js
+++ js/zawhax.js
@@ -1,6 +1,17 @@
+
 $(document).ready(()=> {
     $("img").each(function() {
+        $(this).addClass("img-thumbnail");
+    });
+    $(".separator").each(function() {
         $(this).addClass("rounded");
         $(this).addClass("shadow");
-    });    
+        $(this).addClass("bg-dark");
+    });
+    $(".post").each(function() {
+        $(this).addClass("shadow");
+        $(this).addClass("p-4");
+    });
+    $("#header-inner").addClass("shadow");
+    $("#header-inner").addClass("rounded");
 })
